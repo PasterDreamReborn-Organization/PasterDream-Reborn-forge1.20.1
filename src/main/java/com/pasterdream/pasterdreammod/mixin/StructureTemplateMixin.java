360add53f917350ead2cf024853ea411b4e09972
2884omgpy <1357834094@qq.com>
2026-07-25 15:33:16 +0800

    修复结构含水问题

---
+++ src/main/java/com/pasterdream/pasterdreammod/mixin/StructureTemplateMixin.java
@@ -0,0 +1,38 @@
+package com.pasterdream.pasterdreammod.mixin;
+
+
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(StructureTemplate.class)
+public class StructureTemplateMixin
+{
+    private static final ThreadLocal<Boolean> FORCE_NO_KEEP_LIQUIDS = new ThreadLocal<>();
+
+    @Inject(method = "placeInWorld", at = @At("HEAD"))
+    private void setNoKeepLiquids(CallbackInfoReturnable<Boolean> cir)
+    {
+        FORCE_NO_KEEP_LIQUIDS.set(true);
+    }
+
+    @Inject(method = "placeInWorld", at = @At("RETURN"))
+    private void clearNoKeepLiquids(CallbackInfoReturnable<Boolean> cir)
+    {
+        FORCE_NO_KEEP_LIQUIDS.remove();
+    }
+
+    @Redirect(method = "placeInWorld", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/levelgen/structure/templatesystem/StructurePlaceSettings;shouldKeepLiquids()Z"))
+    private boolean forceNoKeepLiquids(StructurePlaceSettings settings)
+    {
+        if (FORCE_NO_KEEP_LIQUIDS.get() != null)
+        {
+            return false;
+        }
+        return settings.shouldKeepLiquids();
+    }
+}
