1235bc0c5cccd1d9730d85bce8ce622eccec81e1
ShiLiuYinYu <ShiLiuYinYu@users.noreply.github.com>
2026-07-29 23:22:29 +0800

    fix(worldgen): 修复染梦裂缝结构生成配置
    - 将染梦裂缝结构的生成稀疏度从 64 调整为 48

--- src/main/java/com/pasterdream/pasterdreammod/worldgen/structures/ModStructureConfig.java
+++ src/main/java/com/pasterdream/pasterdreammod/worldgen/structures/ModStructureConfig.java
@@ -32,7 +32,7 @@ public class ModStructureConfig
         // === 主世界独立结构 ===
         STRUCTURES.add(new StructureGenerationConfig("pasterdream:oak_fisherman_hut", "pasterdream:can_fisherman_hut_spawn_biome", "surface_structures", "beard_thin", 0, "WORLD_SURFACE_WG", 1, 1, false, "rigid", "minecraft:empty", 1, 25, 8, 15673946, 1));
         STRUCTURES.add(new StructureGenerationConfig("pasterdream:spruce_fisherman_hut", "pasterdream:can_fisherman_hut_spawn_biome", "surface_structures", "beard_thin", 0, "WORLD_SURFACE_WG", 1, 1, false, "rigid", "minecraft:empty", 1, 47, 8, 56179235, 1));
-        STRUCTURES.add(new StructureGenerationConfig("pasterdream:dyedream_crack", "minecraft:is_overworld", "surface_structures", "none", 64, "WORLD_SURFACE_WG", 1, 1, false, "rigid", "minecraft:empty", 1, 64, 16, 33554432, 1));
+        STRUCTURES.add(new StructureGenerationConfig("pasterdream:dyedream_crack", "minecraft:is_overworld", "surface_structures", "none", 64, "WORLD_SURFACE_WG", 1, 1, false, "rigid", "minecraft:empty", 1, 48, 16, 33554432, 1));
         STRUCTURES.add(new StructureGenerationConfig("pasterdream:campsite_overworld", "pasterdream:can_campsite_spawn_biome", "surface_structures", "beard_thin", 0, "WORLD_SURFACE_WG", 1, 1, false, "rigid", "minecraft:empty", 1, 23, 8, 72918463, 1));
         STRUCTURES.add(new StructureGenerationConfig("pasterdream:the_lost_sword_tomb", "minecraft:is_jungle", "surface_structures", "beard_thin", -2, "WORLD_SURFACE_WG", 1, 1, false, "rigid", "minecraft:empty", 1, 32, 8, 41827365, 1));
 
