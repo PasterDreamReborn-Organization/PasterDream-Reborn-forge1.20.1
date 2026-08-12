38ef417a561024c3ed2dcf62cb3feb3fe0f8d2fd
ShiLiuYinYu <ShiLiuYinYu@users.noreply.github.com>
2026-08-12 17:13:42 +0800

    feat(structure): 添加阴影巢穴结构
    - 在英语语言文件中添加阴影巢穴名称定义
    - 在结构配置中注册新的阴影巢穴生成配置
    - 在中文语言文件中添加阴影巢穴名称定义
    - 配置阴影巢穴在阴影森林生物群系中的生成规则
    - 设置阴影巢穴的结构生成参数和权重值

--- src/main/java/com/pasterdream/pasterdreammod/datagen/lang/ModZhCnLangProvider.java
+++ src/main/java/com/pasterdream/pasterdreammod/datagen/lang/ModZhCnLangProvider.java
@@ -2024,6 +2024,7 @@ public class ModZhCnLangProvider extends LanguageProvider {
         add("structure.pasterdream.shadownote_ruin_1", "阴影笔记废墟");
         add("structure.pasterdream.shadownote_ruin_2", "阴影笔记废墟");
         add("structure.pasterdream.shadow_shelter", "阴影庇护所");
+        add("structure.pasterdream.shadow_nest", "阴影巢穴");
         add("structure.pasterdream.invaded_church", "侵染教堂");
         add("structure.pasterdream.twilight_lantern", "暮影之笼");
         add("message.pasterdream.twilight_lantern.activate_fail_no_knowledge", "你尚未知晓如何激活影灯");
