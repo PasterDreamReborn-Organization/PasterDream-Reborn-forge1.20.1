c94f004c43ae49608539e12e66de43ee8c9ae8ef
ShiLiuYinYu <ShiLiuYinYu@users.noreply.github.com>
2026-08-13 20:07:21 +0800

    style(content): 统一修改笔记标题格式并优化内容
    - 将">被阴影浸染的字迹潦草的笔记<"改为"-被阴影浸染的字迹潦草的笔记-"
    - 更新创意模式标签页中的对应标题显示
    - 修正英文语言文件中的标题格式
    - 修正中文简体语言文件中的标题格式
    - 优化多个梦境笔记内容的排版和表述

--- src/main/java/com/pasterdream/pasterdreammod/init/ModCreativeModeTabs.java
+++ src/main/java/com/pasterdream/pasterdreammod/init/ModCreativeModeTabs.java
@@ -658,7 +658,7 @@ public class ModCreativeModeTabs {
                         output.accept(DreamNotesBookWithNBTToCreativeModeTab.buildNBT("黑暗之地"));
                         output.accept(DreamNotesBookWithNBTToCreativeModeTab.buildNBT("阴影小木屋"));
                         output.accept(DreamNotesBookWithNBTToCreativeModeTab.buildNBT("阴影地下工作室"));
-                        output.accept(DreamNotesBookWithNBTToCreativeModeTab.buildNBT(">被阴影浸染的字迹潦草的笔记<"));
+                        output.accept(DreamNotesBookWithNBTToCreativeModeTab.buildNBT("-被阴影浸染的字迹潦草的笔记-"));
                         output.accept(DreamNotesBookWithNBTToCreativeModeTab.buildNBT("关于黑暗之手的随笔"));
                         output.accept(DreamNotesBookWithNBTToCreativeModeTab.buildNBT("与黑色双手的决战和败北"));
                         output.accept(DreamNotesBookWithNBTToCreativeModeTab.buildNBT("侵染教堂-黑面"));
