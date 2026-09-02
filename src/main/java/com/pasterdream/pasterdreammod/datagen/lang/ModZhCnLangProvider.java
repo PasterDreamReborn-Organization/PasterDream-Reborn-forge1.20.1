d3d9040ec040eeb8661bf58f060ec16efe54baac
ShiLiuYinYu <ShiLiuYinYu@users.noreply.github.com>
2026-09-02 17:45:28 +0800

    fix(lang): 修正魔法石工具提示中的错别字
    - 将"魔法师"改为"魔法使"
    - 将"踹"改为"揣"

--- src/main/java/com/pasterdream/pasterdreammod/datagen/lang/ModZhCnLangProvider.java
+++ src/main/java/com/pasterdream/pasterdreammod/datagen/lang/ModZhCnLangProvider.java
@@ -676,7 +676,7 @@ public class ModZhCnLangProvider extends LanguageProvider {
         add("button.pasterdream.copy_button", "复制");
         add("button.pasterdream.research_button", "研究");
 
-        add("tooltip.pasterdreammod.magic_stone", "§7§o哪个魔法师的兜里不会踹几块魔法石呢？");
+        add("tooltip.pasterdreammod.magic_stone", "§7§o哪个魔法使的兜里不会揣几块魔法石呢？");
         add("tooltip.pasterdreammod.goldenrod_tea", "§d持续移除饥饿与反胃效果");
         add("tooltip.pasterdreammod.glass_cup_of_honey_juice", "§d饮用后清空所有负面效果");
         add("tooltip.pasterdreammod.dyedream_perfume", "§d清空未睡眠天数，幻翼不会攻击你");
