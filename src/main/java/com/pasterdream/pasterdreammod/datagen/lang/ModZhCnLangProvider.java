eba9ac9b7bbf4b11874f601b75288da4d9404cea
2884omgpy <1357834094@qq.com>
2026-09-11 01:09:10 +0800

    将暗影地牢结构生成修改为配置项

--- src/main/java/com/pasterdream/pasterdreammod/datagen/lang/ModZhCnLangProvider.java
+++ src/main/java/com/pasterdream/pasterdreammod/datagen/lang/ModZhCnLangProvider.java
@@ -1735,6 +1735,7 @@ public class ModZhCnLangProvider extends LanguageProvider {
         add("message.pasterdream.broken_portal.此暗影地牢中存在其他玩家", "此暗影地牢中存在其他玩家，请稍后再来或寻找并使用其他暗影地牢传送门");
         add("message.pasterdream.broken_portal.传送倒计时：", "传送倒计时：");
         add("message.pasterdream.broken_portal.已传送出暗影地牢内的掉落物", "已传送出暗影地牢内的掉落物");
+        add("message.pasterdream.broken_portal.已传送出或清除暗影地牢内的其他生物", "已传送出或清除暗影地牢内的其他生物");
 
         //卡莱的调料瓶
         add(ModItems.CALAIS_SPICE_BOTTLE.get(), "卡莱的调料瓶");
